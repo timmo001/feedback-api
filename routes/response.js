c3050e57b7158f2e2c92d72b36722e9668414784
Timmo <contact@timmo.xyz>
2018-09-07 10:47:07 +0100

    :sparkles: add database to store responses

--- routes/response.js
+++ routes/response.js
@@ -1,15 +1,31 @@
 const express = require('express'),
   router = express.Router();
+loki = require('lokijs');
+
+var users;
+const databaseInitialize = () => {
+  users = db.getCollection('users')
+  if (!users) users = db.addCollection('users');
+};
+
+var db = new loki('files/home-panel.db', {
+  autoload: true,
+  autoloadCallback: databaseInitialize,
+  autosave: true,
+  autosaveInterval: 4000
+});
+
+const addResponse = (response) => {
+  console.log('Adding response to db..');
+  return users.insert(response);
+};
 
 /* POST response */
-router.post('/', (req, res, next) => {
+router.post('/response', (req, res, next) => {
   if (!req.body.id) res.status(400).send('No ID specified');
   else if (req.body.status === undefined) res.status(400).send('No status specified');
   else {
-    console.log('id:', req.body.id);
-    console.log('status:', req.body.status);
-    console.log('comment:', req.body.comment);
-
+    addResponse(req.body);
     res.sendStatus(200);
   }
 });
