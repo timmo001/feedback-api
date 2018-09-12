3af52d2c65eb86b1e164b379aff198d50f639f01
Timmo <contact@timmo.xyz>
2018-09-12 14:40:23 +0100

    :hammer: add cors origin

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(helmet());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const jsonParser = bodyParser.json({ type: 'application/json' });
 
 require('./routes')(app, jsonParser, {});
